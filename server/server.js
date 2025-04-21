39fdb1430441b17c064a98320f6f2c0f1fd861a1
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-04-22 00:09:43 +0300

    add & connect signup route

--- server/server.js
+++ server/server.js
@@ -25,3 +25,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+
+// Use routes
+app.use('/api', authRoutes);
