98392c786fa3d18fb32f271c805009fdccc3c782
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-04-22 00:50:55 +0300

    update authRoutes to handle protected routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
-
+router.get('/profile', protect, (req, res) => {
+    res.json(req.user);
+  });
+  
 module.exports = router;
