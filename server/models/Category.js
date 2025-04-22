bf72a6066f5a2e13347adfc028f67942acfe6132
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-04-22 12:05:30 +0300

    install slugify & define category model

---
+++ server/models/Category.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Category name is required'],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Auto-generate slug before saving
+categorySchema.pre('validate', function (next) {
+  if (this.name) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+module.exports = mongoose.model('Category', categorySchema);
