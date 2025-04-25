a35c7e32d2cf37f1d87e2c21b606c7c503084af7
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-04-25 20:46:33 +0300

    create and define order model

---
+++ server/models/Order.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    items: [orderItemSchema],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['Mpesa', 'PayPal', 'Card'],
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ['pending', 'paid'],
+      default: 'pending',
+    },
+    orderStatus: {
+      type: String,
+      enum: ['processing', 'shipped', 'delivered'],
+      default: 'processing',
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    paidAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', orderSchema);
